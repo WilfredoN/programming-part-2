52711a00de016a86e1fb415dc27842c0713c7bf1
capynq <thekillermaps@gmail.com>
2025-04-21 17:07:15 +0300

    feat: lab 2 base structure

---
+++ Lab2_2/lib/private/math_utils.cpp
@@ -0,0 +1,3 @@
+#include "math_utils.hpp"
+
+int add(int a, int b) { return a + b; }
---
+++ Lab2_2/main.cpp
@@ -0,0 +1,7 @@
+#include "iostream"
+#include "math_utils.hpp"
+
+int main(void) {
+  std::cout << "4 + 5 = " << add(4, 5) << std::endl;
+  return 0;
+}
