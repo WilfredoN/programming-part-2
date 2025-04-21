8c22dff6ebf63c9855ff23ffde63f01d11feaad1
capynq <thekillermaps@gmail.com>
2025-04-21 17:33:42 +0300

    feat: base unit tests

--- Lab2_2/main.cpp
+++ Lab2_2/main.cpp
@@ -1,7 +1,16 @@
 #include "iostream"
 #include "math_utils.hpp"
+#include <cassert>
 
 int main(void) {
-  std::cout << "4 + 5 = " << add(4, 5) << std::endl;
+  int a = 4;
+  int b = 5;
+  int c = add(a, b);
+
+  std::cout << a << " + " << b << " = " << c << std::endl;
+
+  assert(("The number should be 9", c == 9));
+  assert(("The number should be 9", c == 8));
+
   return 0;
 }
